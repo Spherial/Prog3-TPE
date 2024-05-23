6993ca5787f65d1bd3d5af817f034ae5087df57e
Spherial <xsub123@gmail.com>
2024-05-23 20:58:17 -0300

    Agregado esqueleto de clase Servicios

---
+++ src/Servicios.java
@@ -0,0 +1,26 @@
+public class Servicios {
+//Completar con las estructuras y métodos privados que se requieran
+
+    //Expresar la complejidad temporal del constructor.
+
+    public Servicios(String pathProcesadores, String pathTareas) {
+    }
+
+     //Expresar la complejidad temporal del servicio 1.
+
+    public Tarea servicio1(String ID) {
+        return null;
+    }
+
+    //Expresar la complejidad temporal del servicio 2.
+
+    public List<Tarea> servicio2(boolean esCritica) {
+        return null;
+    }
+
+    //Expresar la complejidad temporal del servicio 3.
+
+    public List<Tarea> servicio3(int prioridadInferior, int  prioridadSuperior) {
+        return null;
+    }
+}
