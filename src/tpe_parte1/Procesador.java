dc2f24373a84f6463908d41416e1ae9bf0a2252a
Spherial <xsub123@gmail.com>
2024-05-24 16:28:59 -0300

    Agregadas clases Tarea y Procesador

--- src/tpe_parte1/Procesador.java
+++ src/tpe_parte1/Procesador.java
@@ -1,2 +1,33 @@
-package tpe_parte1;public class Procesador {
+package tpe_parte1;
+
+public class Procesador {
+    private String id;
+    private String codigo;
+    private boolean estaRefrigerado;
+    private int anioFuncionamiento;
+
+
+
+    public Procesador(String id, String codigo, boolean estaRefrigerado, int anioFuncionamiento) {
+        this.id = id;
+        this.codigo = codigo;
+        this.estaRefrigerado = estaRefrigerado;
+        this.anioFuncionamiento = anioFuncionamiento;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public boolean isEstaRefrigerado() {
+        return estaRefrigerado;
+    }
+
+    public int getAnioFuncionamiento() {
+        return anioFuncionamiento;
+    }
 }
--- src/tpe_parte1/Tarea.java
+++ src/tpe_parte1/Tarea.java
@@ -1,4 +1,41 @@
 package tpe_parte1;
 
 public class Tarea {
+
+    private String id;
+    private String nombre;
+    private int tiempoEjecucion;
+    private boolean esCritica;
+    private int prioridad;
+
+
+    public Tarea(String id, String nombre, int tiempoEjecucion, boolean esCritica, int prioridad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.tiempoEjecucion = tiempoEjecucion;
+        this.esCritica = esCritica;
+        this.prioridad = prioridad;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getTiempoEjecucion() {
+        return tiempoEjecucion;
+    }
+
+    public boolean EsCritica() {
+        return esCritica;
+    }
+
+    public int getPrioridad() {
+        return prioridad;
+    }
+
+
 }
