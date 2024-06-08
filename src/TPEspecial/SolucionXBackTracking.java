ad4e3d8e6d74d504d6f0caa8f74e930cb0df5440
Spherial <xsub123@gmail.com>
2024-06-08 15:38:30 -0300

    Cambiada la forma en la que se inicializa la clase y se asignan tareas

--- src/TPEspecial/SolucionXBackTracking.java
+++ src/TPEspecial/SolucionXBackTracking.java
@@ -2,28 +2,32 @@ package TPEspecial;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class SolucionXBackTracking {
     private HashMap<Procesador, ArrayList<Tarea>> asignaciones;
+    private List<Procesador> procesadores;
+    private int tiempoMax;
 
 
-    public SolucionXBackTracking() {
+    public SolucionXBackTracking(List<Procesador> procesadores, int tiempoMax) {
+        this.procesadores = procesadores;
+        this.tiempoMax = tiempoMax;
         asignaciones = new HashMap<>();
+
+
+
+        for(Procesador p : procesadores){
+            ArrayList<Tarea> t = new ArrayList<>();
+            this.asignaciones.put(p, t);
+        }
     }
 
 
     //Asigna una tarea a un procesador (construye solucion parcial)
     public void asignarTarea(Procesador procesador, Tarea tarea, int x) {
-        // Si el procesador no tiene lista de tareas (es uno nuevo) la creo
-        if (!asignaciones.containsKey(procesador)) {
-            asignaciones.put(procesador, new ArrayList<>());
-        }
+        this.asignaciones.get(procesador).add(tarea);
 
-
-        // Si ese procesador ya tenia una lista de tareas asignadas, y es asignable, le agrego la tarea
-        if (esAsignable(procesador, tarea, x)) {
-            asignaciones.get(procesador).add(tarea);
-        }
 
 
     }
@@ -61,18 +65,6 @@ public class SolucionXBackTracking {
         return tiempo;
     }
 
-    //Comprueba si a un procesador se le puede asignar cierta tarea
-    public boolean esAsignable(Procesador procesador, Tarea tarea, int x){
-        if (procesador.estaRefrigerado() && tarea.getTiempoEjecucion() > x){
-            return false;
-        }
-
-        if (tarea.EsCritica()  &&  getCantCriticas(procesador) >= 2){
-            return false;
-        }
-
-        return true;
-    }
 
 
 
@@ -87,6 +79,10 @@ public class SolucionXBackTracking {
 
         return cantidad;
     }
+
+
+
+
 
 
 }
